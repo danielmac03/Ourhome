db07078dab11b04cee55b033020410a4f3300b8d
Daniel Macías <dani-mac@hotmail.es>
2020-09-06 12:45:45 +0200

    Subir proyecto Spring

---
+++ Source/OURHOME-API/src/main/java/com/ourhome/OurhomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.ourhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurhomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurhomeApiApplication.class, args);
+	}
+
+}
