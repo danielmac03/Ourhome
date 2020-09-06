b636db33e7a357636906123648a261d5a1f53f00
Daniel Macías <dani-mac@hotmail.es>
2020-09-06 15:09:33 +0200

    Mejoras db

--- Base de datos/OURHOME.sql
+++ Base de datos/OURHOME.sql
@@ -1,52 +1,24 @@
-
-CREATE DATABASE IF NOT EXISTS ourhome;
+DROP DATABASE IF EXISTS ourhome;
+CREATE DATABASE ourhome;
 USE ourhome;
-DROP TABLE IF EXISTS usuarios;
-DROP TABLE IF EXISTS test_respuestas;
-DROP TABLE IF EXISTS respuestas_usuarios;
-DROP TABLE IF EXISTS questionarios;
-DROP TABLE IF EXISTS casas;
-DROP TABLE IF EXISTS relaciones;
 
-
-CREATE TABLE IF NOT EXISTS usuarios(
+CREATE TABLE usuarios(
 	id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) DEFAULT NULL,
     edad INT NOT NULL,
-    telefono INT (9) NOT NULL,
+    telefono INT NOT NULL,
     correo VARCHAR(100) NOT NULL,
     contraseña VARCHAR(100) NOT NULL,
     rol ENUM('Con Casa', 'Sin Casa') NOT NULL,
-    fecha_registro date DEFAULT NULL,
-    contacto ENUM('Mostrar Correo', 'Mostrar Telefono') NOT NULL,
-    respuestas_test_defecto VARCHAR(100) DEFAULT NULL
+	respuestas_test_defecto JSON NOT NULL,
+    mostrar_correo bit DEFAULT NULL,
+	mostrar_telefono bit DEFAULT NULL,
+	fecha_registro date DEFAULT NULL,
+	fecha_actualizacion date DEFAULT NULL
 );
 
-CREATE TABLE IF NOT EXISTS test_respuestas(
-	id_test_respuestas INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_test INT NOT NULL,
-    id_respuestas_usuario INT NOT NULL,
-    respuestas VARCHAR(100) NOT NULL,
-    compatibilidad INT
-);
-
-CREATE TABLE IF NOT EXISTS respuestas_usuarios(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT NOT NULL,
-    id_test_respuestas INT NOT NULL,
-	FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (id_test_respuestas) REFERENCES test_respuestas(id_test_respuestas)
-);
-
-CREATE TABLE IF NOT EXISTS questionarios(
-	id_test INT PRIMARY KEY,
-    id_usuario_creador INT NOT NULL,
-    respuestas_correctas VARCHAR(100),
-    FOREIGN KEY (id_usuario_creador) REFERENCES usuarios(id_usuario)
-);
-
-CREATE TABLE IF NOT EXISTS casas(
+CREATE TABLE casas(
 	id_casa INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_usuario INT NOT NULL,
     descripcion VARCHAR(500) DEFAULT NULL,
@@ -54,7 +26,7 @@ CREATE TABLE IF NOT EXISTS casas(
     numero_habitaciones INT NOT NULL,
     numero_baños INT NOT NULL,
     direccion VARCHAR(100) DEFAULT NULL,
-    metros2 INT DEFAULT NULL,
+    metros INT DEFAULT NULL,
     pisos INT NOT NULL,
     parquing ENUM('Si', 'No') NOT NULL,
     calefaccion ENUM('Si', 'No') NOT NULL,
@@ -62,9 +34,28 @@ CREATE TABLE IF NOT EXISTS casas(
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
-CREATE TABLE IF NOT EXISTS relaciones(
-	id_relacion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE proceso(
+	id_proceso INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_usuario_1 INT NOT NULL,
     id_usuario_2 INT NOT NULL,
-    proceso ENUM('En Contacto', 'Cancelado', 'Finalizado') NOT NULL
+    estado ENUM('En Contacto', 'Cancelado', 'Finalizado') NOT NULL,
+    FOREIGN KEY (id_usuario_1) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (id_usuario_2) REFERENCES usuarios(id_usuario)
+);
+
+CREATE TABLE test_personalizados(
+	id_test_personalizados INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario_creador INT NOT NULL,
+    respuestas_correctas JSON NOT NULL,
+    FOREIGN KEY (id_usuario_creador) REFERENCES usuarios(id_usuario)
+);
+
+CREATE TABLE respuestas_test_personalizado(
+	id_respuestas_test_personalizado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    id_test_personalizado INT NOT NULL,
+    respuesta JSON NOT NULL,
+    compatibilidad DECIMAL NOT NULL,
+	FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (id_test_personalizado) REFERENCES test_personalizados(id_test_personalizados)
 );
