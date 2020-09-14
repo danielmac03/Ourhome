94198fc3dba1b4ef8b2b4995139f7514a21adcac
Arnau <arnau.colome,estivill@gmail.com>
2020-09-14 20:33:51 +0200

    bk

--- Source/OURHOME-API/src/main/java/com/ourhome/controller/TestsPersonalizadosController.java
+++ Source/OURHOME-API/src/main/java/com/ourhome/controller/TestsPersonalizadosController.java
@@ -1,5 +1,59 @@
 package com.ourhome.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.ourhome.dto.TestsPersonalizados;
+import com.ourhome.service.TestsPersonalizadosServiceImpl;
+
+@RestController
+@RequestMapping("/api")
 public class TestsPersonalizadosController {
+
+	@Autowired
+	TestsPersonalizadosServiceImpl testsPersonalizadosServiceImpl;
+	
+	@GetMapping("/TestsPersonalizados")
+	public List<TestsPersonalizados> listarTestsPersonalizados(){
+		return TestsPersonalizadosServiceImpl.listarTestsPersonalizados();
+	}
+
+	@PostMapping("/TestsPersonalizados")
+	public TestsPersonalizados guardarTestPersonalizado(@RequestBody TestsPersonalizados testPersonalizado) {
+		return TestsPersonalizadosServiceImpl.guardarTestPersonalizado(testPersonalizado);
+	}
+	
+	@GetMapping("/TestsPersonalizados/{id}")
+	public TestsPersonalizados buscarProceso(@PathVariable(name="id") int id) {
+		TestsPersonalizados TestsPersonalizado = new TestsPersonalizados();		
+		TestsPersonalizado = TestsPersonalizadosServiceImpl.buscarTestPersonalizado(id);
+		
+		return TestsPersonalizado;
+	}
+	
+	@PutMapping("/departamentos/{id}")
+	public TestsPersonalizados actualizarTestPersonalizado(@PathVariable(name="id") int id,@RequestBody TestsPersonalizados TestsPersonalizado) {
+		TestsPersonalizados testsPersonalizadosSeleccionado = new TestsPersonalizados();
+		TestsPersonalizados testsPersonalizadosActualizado= new TestsPersonalizados();
+		
+		testsPersonalizadosSeleccionado= TestsPersonalizadosServiceImpl.buscarTestPersonalizado(id);
+		
+		testsPersonalizadosSeleccionado.setId_test_personalizados(TestsPersonalizados.getId_test_personalizados());
+		testsPersonalizadosSeleccionado.setId_usuario_creador(TestsPersonalizados.getId_usuario_creador());
+		testsPersonalizadosSeleccionado.setRespuestas_correctas(TestsPersonalizados.getRespuestas_correctas());
+		testsPersonalizadosSeleccionado.setMinimas_respuestas_correctas(TestsPersonalizados.getMinimas_respuestas_correctas());
+		testsPersonalizadosSeleccionado.setFecha_creacion(TestsPersonalizados.getFecha_creacion());
+		testsPersonalizadosSeleccionado.setRespuestasTestsPersonalizados(TestsPersonalizados.getRespuestasTestsPersonalizados());
+
+		testsPersonalizadosActualizado = TestsPersonalizadosServiceImpl.actualizarTestPersonalizado(testsPersonalizadosSeleccionado);
+		
+		return testsPersonalizadosActualizado;
+	}
+
+	
+	@DeleteMapping("/TestsPersonalizados/{id}")
+	public void eliminarTestPersonalizado(@PathVariable(name="id") int id) {
+		TestsPersonalizadosServiceImpl.eliminarTestPersonalizado(id);
+	}
 
 }
--- Source/OURHOME-API/src/main/java/com/ourhome/controller/UsuariosController.java
+++ Source/OURHOME-API/src/main/java/com/ourhome/controller/UsuariosController.java
@@ -1,8 +1,13 @@
 package com.ourhome.controller;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.ourhome.dto.Usuarios;
+import com.ourhome.service.UsuariosServiceImpl;
 
+@RestController
+@RequestMapping("/api")
 public class UsuariosController {
 
 }
