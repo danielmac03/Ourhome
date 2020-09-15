a22ed335e8509a7d3f2f406aec6e8b3997cca600
Alba Fischer <albafischer3@gmail.com>
2020-09-15 17:37:41 +0200

    Base de datos

--- Base de datos/OURHOME.sql
+++ Base de datos/OURHOME.sql
@@ -11,7 +11,7 @@ CREATE TABLE usuarios(
     correo VARCHAR(100) NOT NULL,
     contraseña VARCHAR(100) NOT NULL,
     rol ENUM('Con Casa', 'Sin Casa') NOT NULL,
-	respuestas_test_defecto JSON NOT NULL,
+	respuestas_test_defecto MEDIUMTEXT NOT NULL,
     mostrar_correo bit DEFAULT NULL,
 	mostrar_telefono bit DEFAULT NULL,
 	fecha_creacion DATE DEFAULT NULL,
@@ -21,14 +21,14 @@ CREATE TABLE usuarios(
 CREATE TABLE casas(
 	id_casa INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_usuario INT NOT NULL,
-    descripcion VARCHAR(500) DEFAULT NULL,
+    descripcion MEDIUMTEXT DEFAULT NULL,
     precio DOUBLE NOT NULL,
     numero_habitaciones INT NOT NULL,
     numero_baños INT NOT NULL,
     direccion VARCHAR(100) DEFAULT NULL,
     metros DECIMAL DEFAULT NULL,
     plantas INT NOT NULL,
-	extras JSON DEFAULT NULL,
+	extras MEDIUMTEXT DEFAULT NULL,
 	fecha_creacion DATE DEFAULT NULL,
 	fecha_actualizacion DATE DEFAULT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
@@ -48,7 +48,7 @@ CREATE TABLE procesos(
 CREATE TABLE test_personalizados(
 	id_test_personalizados INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_usuario_creador INT NOT NULL,
-    respuestas_correctas JSON NOT NULL,
+    respuestas_correctas MEDIUMTEXT NOT NULL,
     minimas_respuestas_correctas INT NOT NULL,
 	fecha_creacion DATE DEFAULT NULL,
     FOREIGN KEY (id_usuario_creador) REFERENCES usuarios(id_usuario)
@@ -58,7 +58,7 @@ CREATE TABLE respuestas_tests_personalizados(
 	id_respuestas_test_personalizado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT NOT NULL,
     id_test_personalizado INT NOT NULL,
-    respuestas JSON NOT NULL,
+    respuestas MEDIUMTEXT NOT NULL,
     compatibilidad DECIMAL NOT NULL,
 	fecha_creacion DATE DEFAULT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
