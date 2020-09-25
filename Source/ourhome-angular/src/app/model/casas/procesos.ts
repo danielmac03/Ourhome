01955150a546e7ba468c1a4a4f4dd8aeb39f23d7
Arnau <arnau.colome,estivill@gmail.com>
2020-09-25 22:52:52 +0200

    subir model

---
+++ Source/ourhome-angular/src/app/model/casas/procesos.ts
@@ -0,0 +1,8 @@
+export class respuestas_tests_personalizados {
+  id_proceso: number;
+  id_usuario_1: number;
+  id_usuario_2: number;
+  estado: string;
+  fecha_creacion: Date;
+  fecha_actualizacion: Date;
+}
---
+++ Source/ourhome-angular/src/app/model/casas/respuestas_tests_personalizados.ts
@@ -0,0 +1,8 @@
+export class respuestas_tests_personalizados {
+  id_respuestas_test_personalizado: number;
+  id_usuario: number;
+  id_test_personalizado: number;
+  respuestas: string;
+  compatibilidad: number;
+  fecha_creacion: Date;
+}
