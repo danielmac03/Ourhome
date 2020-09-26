beff07978c410b4f32e4f03c165cabc896c3dcb3
Arnau C <arnau.colome.estivill@gmail.com>
2020-09-26 09:57:27 +0000

    Update OURHOME.sql

--- Base de datos/OURHOME.sql
+++ Base de datos/OURHOME.sql
@@ -2,22 +2,22 @@ DROP DATABASE IF EXISTS ourhome;
 CREATE DATABASE ourhome;
 USE ourhome;
 
-CREATE TABLE usuarios(
-    id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellidos VARCHAR(100) DEFAULT NULL,
-    edad INT NOT NULL,
-    telefono INT NULL,
-    correo VARCHAR(100) NOT NULL,
-    contraseña VARCHAR(100) NOT NULL,
-    rol ENUM('tengo_casa', 'busco_casa', 'admin') NOT NULL,
-    respuestas_test_defecto MEDIUMTEXT NOT NULL,
-    mostrar_telefono bit DEFAULT NULL,
-    fecha_creacion DATE NOT NULL,
-    fecha_actualizacion DATE NOT NULL
+CREATE TABLE users(
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    surnames VARCHAR(100) DEFAULT NULL,
+    age INT NOT NULL,
+    phone INT NULL,
+    mail VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role ENUM('tengo_casa', 'busco_casa', 'admin') NOT NULL,
+    default_test_responses MEDIUMTEXT NOT NULL,
+    show_phone bit DEFAULT NULL,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL
 );
 
-INSERT INTO usuarios (nombre, apellidos, edad, telefono, correo, contraseña, rol, respuestas_test_defecto, mostrar_telefono, fecha_creacion, fecha_actualizacion) VALUES 
+INSERT INTO users (name, surname, age, phone, mail, password, role, default_test_responses, show_phone, created_at, updated_at) VALUES
 ('Daniel', 'Rodrigues', 15, 699873544, 'danielrodrigues@gmail.com', '$2a$10$XURPShQNCsLjp1ESc2laoObo9QZDhxz73hJPaEv7/cBha4pk0AgP.', '2', 'as', 1, CURRENT_DATE(), CURRENT_DATE()),
 ('Marco', 'Polo', 20, 633254182, 'marcololo@hotmail.com', 'marco00', '1', 'sa', 0, CURRENT_DATE(), CURRENT_DATE()),
 ('Anna', 'mota', 25, 124512325, 'annamota@gmail.com', 'anna00', '2', 'as', 1, CURRENT_DATE(), CURRENT_DATE()),
@@ -29,24 +29,24 @@ INSERT INTO usuarios (nombre, apellidos, edad, telefono, correo, contraseña, ro
 ('Amapola', 'Guarana', 18, 188526785, 'amapolaguarana@hotmail.com', 'amapola00', '2', 'sa', 0, CURRENT_DATE(), CURRENT_DATE()),
 ('Paco', 'Marquez', 19, 632251856, 'pacomarquez@gmail.com', 'paco00', '2', 'sa', 0, CURRENT_DATE(), CURRENT_DATE());
 
-CREATE TABLE casas(
-	id_casa INT AUTO_INCREMENT PRIMARY KEY,
-	id_usuario INT NOT NULL,
-    url_fotos MEDIUMTEXT DEFAULT NULL,
-    descripcion MEDIUMTEXT DEFAULT NULL,
-    precio DOUBLE NOT NULL,
-    numero_habitaciones INT NOT NULL,
-    numero_banos INT NOT NULL,
-    direccion VARCHAR(100) DEFAULT NULL,
-    metros DECIMAL DEFAULT NULL,
-    plantas INT NOT NULL,
-	extras MEDIUMTEXT DEFAULT NULL,
-	fecha_creacion DATE NOT NULL,
-	fecha_actualizacion DATE DEFAULT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+CREATE TABLE homes(
+    home_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    url_photos MEDIUMTEXT DEFAULT NULL,
+    description MEDIUMTEXT DEFAULT NULL,
+    price DOUBLE NOT NULL,
+    num_bedrooms INT NOT NULL,
+    num_bathroom INT NOT NULL,
+    direction VARCHAR(100) DEFAULT NULL,
+    meters DECIMAL DEFAULT NULL,
+    floors INT NOT NULL,
+    additional MEDIUMTEXT DEFAULT NULL,
+    created_at DATE NOT NULL,
+    updated_at DATE DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO casas (id_usuario, url_fotos, descripcion, precio, numero_habitaciones, numero_banos, direccion, metros, plantas, fecha_creacion, fecha_actualizacion) VALUES
+INSERT INTO casas (user_id, url_photos, description, price, num_bedrooms, num_bathroom, direction, meters, floors, additional, created_at, updated_at) VALUES
 (10, 'https://imgur.com/a/9kTvflF', 'Lorem Ipsum es simplemente texto de relleno de la industria de la impresión y la composición tipográfica.', 350, 2, 1, 'C/ Tarragona', 120, 1, CURRENT_DATE(), CURRENT_DATE()),
 (8, 'https://imgur.com/a/9kTvflF', 'Lorem Ipsum es simplemente texto de relleno de la industria de la impresión y la composición tipográfica.', 500, 4, 2, 'C/ San Pere', 220, 3, CURRENT_DATE(), CURRENT_DATE()),
 (5, 'https://imgur.com/a/9kTvflF', 'Lorem Ipsum es simplemente texto de relleno de la industria de la impresión y la composición tipográfica.', 400, 3, 1, 'C/ Alcalá', 155, 1, CURRENT_DATE(), CURRENT_DATE()),
@@ -58,18 +58,18 @@ INSERT INTO casas (id_usuario, url_fotos, descripcion, precio, numero_habitacion
 (9, 'https://imgur.com/a/9kTvflF', 'Lorem Ipsum es simplemente texto de relleno de la industria de la impresión y la composición tipográfica.', 525, 6, 3, 'Avenida Constitución ', 200, 2, CURRENT_DATE(), CURRENT_DATE()),
 (2, 'https://imgur.com/a/9kTvflF', 'Lorem Ipsum es simplemente texto de relleno de la industria de la impresión y la composición tipográfica.', 445, 2, 2, 'C/ de la Compañia', 150, 2, CURRENT_DATE(), CURRENT_DATE());
 
-CREATE TABLE procesos(
-	id_proceso INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_usuario_1 INT NOT NULL,
-    id_usuario_2 INT NOT NULL,
-    estado ENUM('En Contacto', 'Cancelado', 'Finalizado') NOT NULL,
-	fecha_creacion DATE DEFAULT NULL,
-	fecha_actualizacion DATE DEFAULT NULL,
-    FOREIGN KEY (id_usuario_1) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (id_usuario_2) REFERENCES usuarios(id_usuario)
+CREATE TABLE processes(
+    process_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id_1 INT NOT NULL,
+    user_id_2 INT NOT NULL,
+    state ENUM('En Contacto', 'Cancelado', 'Finalizado') NOT NULL,
+    created_at DATE DEFAULT NULL,
+    updated_at DATE DEFAULT NULL,
+    FOREIGN KEY (user_id_1) REFERENCES users(user_id),
+    FOREIGN KEY (user_id_2) REFERENCES users(user_id)
 );
 
-INSERT INTO procesos (id_usuario_1, id_usuario_2, estado, fecha_creacion, fecha_actualizacion) VALUES
+INSERT INTO processes (user_id_1 , user_id_2, state, created_at , updated_at) VALUES
 (3, 1, 1, CURRENT_DATE(), CURRENT_DATE()),
 (9, 7, 2, CURRENT_DATE(), CURRENT_DATE()),
 (8, 8, 1, CURRENT_DATE(), CURRENT_DATE()),
@@ -81,16 +81,16 @@ INSERT INTO procesos (id_usuario_1, id_usuario_2, estado, fecha_creacion, fecha_
 (4, 3, 3, CURRENT_DATE(), CURRENT_DATE()),
 (1, 10, 1, CURRENT_DATE(), CURRENT_DATE());
 
-CREATE TABLE test_personalizados(
-	id_test_personalizados INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_usuario_creador INT NOT NULL,
-    respuestas_correctas MEDIUMTEXT NOT NULL,
-    minimas_respuestas_correctas INT NOT NULL,
-	fecha_creacion DATE DEFAULT NULL,
-    FOREIGN KEY (id_usuario_creador) REFERENCES usuarios(id_usuario)
+CREATE TABLE custom_test(
+	custom_test_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    correct_answers MEDIUMTEXT NOT NULL,
+    minimum_correct_responses INT NOT NULL,
+	created_at DATE DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO test_personalizados (id_usuario_creador, respuestas_correctas, minimas_respuestas_correctas, fecha_creacion) VALUES
+INSERT INTO custom_test (user_id, correct_answers, minimum_correct_responses, created_at) VALUES
 (3, 'a', 1, CURRENT_DATE()),
 (3, 'a', 5, CURRENT_DATE()),
 (3, 'a', 6, CURRENT_DATE()),
@@ -102,18 +102,18 @@ INSERT INTO test_personalizados (id_usuario_creador, respuestas_correctas, minim
 (3, 'a', 3, CURRENT_DATE()),
 (3, 'a', 9, CURRENT_DATE());
 
-CREATE TABLE respuestas_tests_personalizados(
-	id_respuestas_test_personalizado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT NOT NULL,
-    id_test_personalizado INT NOT NULL,
-    respuestas MEDIUMTEXT NOT NULL,
-    compatibilidad DECIMAL NOT NULL,
-	fecha_creacion DATE DEFAULT NULL,
-	FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (id_test_personalizado) REFERENCES test_personalizados(id_test_personalizados)
+CREATE TABLE custom_tests_responses(
+	custom_test_response_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    custom_test_id INT NOT NULL,
+    answers MEDIUMTEXT NOT NULL,
+    compatibility DECIMAL NOT NULL,
+	created_at DATE DEFAULT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (custom_test_id) REFERENCES custom_test(custom_test_id)
 );
 
-INSERT INTO respuestas_tests_personalizados (id_usuario, id_test_personalizado, respuestas, compatibilidad, fecha_creacion) VALUES 
+INSERT INTO custom_tests_responses (user_id, custom_test_id, answers, compatibility, created_at) VALUES 
 (1, 10, 'a', 90, CURRENT_DATE()),
 (2, 9, 'a', 30, CURRENT_DATE()),
 (3, 8, 'a', 60, CURRENT_DATE()),
