0a8ac975d4a12c17fc07268ad4e04ecfb81e23e1
Daniel Macías <dani-mac@hotmail.es>
2020-09-29 23:48:53 +0200

    Listar casas

--- Source/ourhome-angular/src/app/model/custom_tests_responses.ts
+++ Source/ourhome-angular/src/app/model/custom_tests_responses.ts
@@ -1,19 +1,8 @@
-import {users} from './users';
-
-import {custom_tests} from './custom_tests';
-
 export class custom_tests_responses {
-
   custom_test_response_id: number;
-
   users: number;
-
   custom_tests: number;
-
   answers: string;
-
   compatibility: number;
-
   created_at: Date;
-
 }
--- Source/ourhome-angular/src/app/model/homes.ts
+++ Source/ourhome-angular/src/app/model/homes.ts
@@ -1,33 +1,16 @@
-import { users } from './users';
-
 export class homes {
-
-  home_id: number;
-
+  id: number;
   users: number;
-
   description: string;
-
   price: number;
-
-  url_photos: string;
-
+  urlPhotos: string;
   num_bedrooms: number;
-
   num_bathroom: number;
-
   city: string;
-
   direction: string;
-
   meters: number;
-
   floors: number;
-
   additional: string;
-
   created_at: Date;
-
   updated_at: Date;
-
 }
