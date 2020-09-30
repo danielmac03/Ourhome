1c5f0f5503df8d2f41b5ddf662bbb94d720ecd76
Arnau <arnau.colome,estivill@gmail.com>
2020-09-30 09:20:19 +0200

    create advertisement

--- Source/ourhome-angular/src/app/create-advertisement/create-advertisement.component.ts
+++ Source/ourhome-angular/src/app/create-advertisement/create-advertisement.component.ts
@@ -1,5 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router } from "@angular/router"
 import { homes } from '../model/homes';
 import { HomesService } from '../service/homes.service';
 
@@ -8,34 +9,29 @@ import { HomesService } from '../service/homes.service';
   templateUrl: './create-advertisement.component.html',
   styleUrls: ['./create-advertisement.component.css', '../app.component.css']
 })
-export class CreateAdvertisementComponent implements OnInit {
+export class CreateAdvertisementComponent implements OnInit{
 
-  homes: homes = new homes();
-  submitted = false;
+ home: homes = new homes();
 
-  constructor(private homesService: HomesService, private router: Router) { }
 
-  ngOnInit() {
+ constructor(private homesService: HomesService, private router: Router) { }
+
+ ngOnInit () { }
+
+  newAdvertisement(): void {
+    this.home = new homes();
   }
 
-  newHome() :  void {
-    this.submitted  = false;
-    this.homes = new homes();
-  }
-
-  save() {
-    this.homesService
-    .createHomes(this.homes).subscribe(data => {
+ save () {
+   this.homesService.createHomes(this.home).subscribe(data => {
       console.log(data)
-      this.homes = new homes();
-      this.router.navigate(['home']);
-      //PONER QUE REENVIE AL ANUNCIO ID
-    },
-    error => console.log(error));
-  }
+      this.home = new homes();
+      this.router.navigate(['/homes']);
+   },
+   error => console.log(error))
+ }
 
-  onSubmit() {
-    this.submitted = true;
+  onSubmit () {
     this.save();
   }
 
