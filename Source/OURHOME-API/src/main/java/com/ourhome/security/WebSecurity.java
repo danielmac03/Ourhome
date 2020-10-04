582eade4fa6b4efe4ef4bb28f8a02a97019f93f0
Daniel Macías <dani-mac@hotmail.es>
2020-10-04 15:49:38 +0200

    Modificar API para obtener información sin iniciar

--- Source/OURHOME-API/src/main/java/com/ourhome/security/WebSecurity.java
+++ Source/OURHOME-API/src/main/java/com/ourhome/security/WebSecurity.java
@@ -38,7 +38,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.cors().and()
 			.csrf().disable()
-			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
+			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL).permitAll().antMatchers(HttpMethod.GET, "/api/homes/**").permitAll()
 			.anyRequest().authenticated().and()
 				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
