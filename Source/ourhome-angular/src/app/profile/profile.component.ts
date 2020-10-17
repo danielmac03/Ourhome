dc33b25e71c56eb9c104349304e6ee3bcffc25d1
Arnau <arnau.colome,estivill@gmail.com>
2020-10-17 22:35:32 +0200

    Funcionalidad mi perfil 2.0

--- Source/ourhome-angular/src/app/profile/profile.component.ts
+++ Source/ourhome-angular/src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TokenStorageService } from '../service/authentication/token-storage.service';
+import {UsersService} from '../service/users.service';
+import {AuthService} from '../service/authentication/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +12,45 @@ import { TokenStorageService } from '../service/authentication/token-storage.ser
 
 export class ProfileComponent implements OnInit {
 
-  constructor(private tokenStorageService: TokenStorageService, private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private tokenStorageService: TokenStorageService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private usersService: UsersService
+  ) {}
 
   user = {} as any;
+
+  name: string;
+  surnames: string;
+  age: number;
+  email: string;
+  phone: number;
+  password: string;
+  role: number;
+  showPhone: number;
 
   ngOnInit() {
     this.user = this.tokenStorageService.getUser();
   }
+
+  update() {
+    const user = {
+      name: this.name,
+      surnames: this.surnames,
+      age: this.age,
+      email: this.email,
+      password: this.password,
+      role: this.role,
+      showPhone: this.showPhone
+    };
+
+    this.tokenStorageService.saveUser(user);
+    this.router.navigate(['initialTest']);
+
+  }
+
+
+
 }
--- Source/ourhome-angular/src/app/register/register.component.ts
+++ Source/ourhome-angular/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from '../service/users.service';
-import { AuthService } from "../service/authentication/auth.service";
+import { AuthService } from '../service/authentication/auth.service';
 import { TokenStorageService } from '../service/authentication/token-storage.service';
 
 @Component({
@@ -9,7 +9,14 @@ import { TokenStorageService } from '../service/authentication/token-storage.ser
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css', '../app.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+    private tokenStorageService: TokenStorageService,
+    private router: Router
+  ) {}
 
   name: string;
   surnames: string;
@@ -20,23 +27,26 @@ export class RegisterComponent implements OnInit {
   role: number;
   showPhone: number;
 
-  constructor(private usersService: UsersService, private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
-
-  ngOnInit() {
-  }
-
   register() {
-    let user = {name: this.name, surnames: this.surnames, age: this.age, email: this.email, password: this.password, role: this.role, showPhone: this.showPhone};
+    const user = {
+      name: this.name,
+      surnames: this.surnames,
+      age: this.age,
+      email: this.email,
+      password: this.password,
+      role: this.role,
+      showPhone: this.showPhone
+    };
 
-    this.authService.login({email: "admin@ourhome.com", password: "password"}).subscribe(resp => {
-      this.tokenStorage.saveToken(resp.headers.get("Authorization"));
+    this.authService.login({email: 'admin@ourhome.com', password: 'password'}).subscribe(resp => {
+      this.tokenStorageService.saveToken(resp.headers.get('Authorization'));
 
       this.usersService.getUserByEmail(this.email).subscribe(resp1 => {
-          if(resp1 == null){
-            this.tokenStorage.saveUser(user);
+          if (resp1 == null){
+            this.tokenStorageService.saveUser(user);
             this.router.navigate(['initialTest']);
           }else{
-            alert("El email ya se ha registrado");
+            alert('El email ya se ha registrado');
           }
         }
       );
