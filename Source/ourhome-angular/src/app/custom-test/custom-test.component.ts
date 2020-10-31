cdd25fcb2631c75e5f5fe8629da45baf687906f4
Daniel Macías <dani-mac@hotmail.es>
2020-10-31 23:30:48 +0100

    Custom test acabado

--- Source/ourhome-angular/src/app/custom-test/custom-test.component.ts
+++ Source/ourhome-angular/src/app/custom-test/custom-test.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import { custom_tests } from '../model/custom_tests';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CustomTestsService } from '../service/custom-tests.service';
+import { TokenStorageService } from '../service/authentication/token-storage.service';
+import { CustomTestsResponsesService } from '../service/custom-tests-responses.service';
 
 @Component({
   selector: 'app-custom-test',
@@ -11,44 +13,65 @@ import { CustomTestsService } from '../service/custom-tests.service';
 })
 export class CustomTestComponent implements OnInit {
 
+  customTest;
   questions = [];
+  answers: number[] = [];
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private customTestsService: CustomTestsService
+    private customTestsService: CustomTestsService,
+    private tokenStorageService: TokenStorageService,
+    private customTestsResponsesService: CustomTestsResponsesService
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void{
     this.customTestsService.getCustomTestsByUser(this.route.snapshot.params.home).subscribe(resp => {
+      this.customTest = resp;
+
       for (let i = 0; i < resp.questions.split(',').length; i++){
         this.questions.push({
           question: resp.questions.split(',')[i],
-          answer: resp.answers.split(',')[i],
           option1: resp.options1.split(',')[i],
           option2: resp.options2.split(',')[i]
         });
       }
+
+      this.answers = resp.answers.split(',');
     }, error => {
       console.log('Error...');
     });
   }
 
-  /*
-  save(): void {
-    this.customTestsService
-    .createCustomTests(this.custom_tests).subscribe(data => {
-      console.log(data)
-      this.custom_tests = new custom_tests();
-      this.router.navigate(['home']);
-    },
-    error => console.log(error));
-  }
+  save(data: NgForm): void{
+    const user = this.tokenStorageService.getUser();
+    const userAnswers = [];
+    let common = 0;
 
-  onSubmit() {
-    this.submitted = true;
-    this.save();
+    for (let i = 0; i < this.questions.length; i++){
+      userAnswers.push(data.value[('question' + i)]);
+    }
+
+    for (let i = 0; i < this.answers.length; i++){
+      if (this.answers[i] === userAnswers[i].toString()){
+        common++;
+      }
+    }
+
+    const customTestResponses = {
+      user,
+      customTest: this.customTest,
+      answers: userAnswers.toString(),
+      compatibility: (common / this.questions.length) * 10
+    };
+
+    this.customTestsResponsesService.createCustomTestsResponses(customTestResponses).subscribe(resp => {
+      console.log('Complete...');
+    }, error => {
+      console.log('Error....');
+    });
+
+    this.router.navigate(['processes']);
   }
-   */
 
 }
