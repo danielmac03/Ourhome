55fcde5b6a15ede984fb9859c55a695864e9af62
danielmac03 <dani-mac@hotmail.es>
2020-12-01 00:04:16 +0100

    Mejorar formulario create-advertisement

--- Source/ourhome-angular/src/app/create-advertisement/create-advertisement.component.ts
+++ Source/ourhome-angular/src/app/create-advertisement/create-advertisement.component.ts
@@ -1,59 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HomesService } from '../service/homes.service';
-import { TokenStorageService } from '../service/authentication/token-storage.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {HomesService} from '../service/homes.service';
+import {TokenStorageService} from '../service/authentication/token-storage.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-create-advertisement',
   templateUrl: './create-advertisement.component.html',
   styleUrls: ['./create-advertisement.component.css']
 })
-export class CreateAdvertisementComponent implements OnInit{
-
-  id: number;
-  description: string;
-  price: number;
-  urlPhotos: string;
-  numBedrooms: number;
-  numBathroom: number;
-  city: string;
-  direction: string;
-  meters: number;
-  floors: number;
-  additional: string;
-  createdAt: Date;
-  updatedAt: Date;
-  user;
+export class CreateAdvertisementComponent implements OnInit {
 
   constructor(
     private homesService: HomesService,
     private router: Router,
     private tokenStorage: TokenStorageService
-  ) { }
+  ) {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
-  save(): void {
-    this.user = this.tokenStorage.getUser();
+  onSubmit(data: NgForm): void {
+    data.value.user = this.tokenStorage.getUser();
+    data.value.active = true;
 
-    const homes = {
-      description: this.description,
-      price: this.price,
-      urlPhotos: this.urlPhotos,
-      numBedrooms: this.numBedrooms,
-      numBathroom: this.numBathroom,
-      city: this.city,
-      direction: this.direction,
-      meters: this.meters,
-      floors: this.floors,
-      additional: this.additional,
-      user: this.user
-    };
-
-    this.homesService.createHomes(homes).subscribe((data: any) => {
+    this.homesService.createHomes(data.value).subscribe(resp => {
       this.router.navigate(['custom-test']);
-    },
-    error => console.log(error));
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
